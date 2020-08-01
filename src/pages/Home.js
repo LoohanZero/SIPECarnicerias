5ae699b644100d530a4901f934099bb233c45e5e
Lujan Sanchez <mlulasan@gmail.com>
2020-08-01 19:32:39 -0300

    verificando responsive 450px

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -16,6 +16,14 @@ const Container = styled.section`
   overflow: hidden;
 
   @media (max-width: 450px) {
+    height: calc(100vh - 120px);
+  }
+
+  @media (max-width: 411px) {
+    height: calc(80vh - 120px);
+  }
+
+  @media (max-width: 375px) {
     height: calc(82vh - 120px);
   }
 `;
